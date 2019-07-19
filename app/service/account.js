e272ce65499fdf331cc581232368c45b85d6969e
kason <718087538@qq.com>
2019-07-19 14:21:05 +0800

    引入md5加密

--- app/service/account.js
+++ app/service/account.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Service = require('egg').Service;
+const md5 = require('blueimp-md5');//引入md5加密
 class AccountService extends Service {
     constructor(ctx) {
         super(ctx);
@@ -8,8 +9,9 @@ class AccountService extends Service {
     
     // 注册
     async account(account, password,nick_name) {
+        let md5Password = md5(password);
         let insertNewsSql = `INSERT INTO public.user(account,password,nick_name) VALUES ($1,$2,$3) RETURNING *;`;
-        let result = await this.app.pg.query(insertNewsSql, [account, password,nick_name]);
+        let result = await this.app.pg.query(insertNewsSql, [account, md5Password,nick_name]);
         this.app.logger.info('result:', result);
         return {
             data: null,
